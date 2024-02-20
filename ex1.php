7900689016a2516ce81332585a158c8e123fe7ff
Vpn210 <victor.canassa@aluno.ifsp.edu.br>
2024-02-20 11:48:03 -0300

    não sei

---
+++ ex1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    div{background-color: purple; font-size: 30px; color: white; padding: 1px;}
+</style>
+<body>
+    <h1>Batata</h1>
+    <div>
+    <?php
+        $nota = 10.5;
+        echo "Nota final: " .$nota;
+    ?>
+    </div>
+</body>
+</html>
