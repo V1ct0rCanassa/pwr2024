017e43ac6e291ae9e2a788132cf2dcccc0e2bd77
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2024-02-21 18:27:18 -0300

    Create ex8.php

---
+++ Lista 1/ex8.php
@@ -0,0 +1,9 @@
+<?php
+    $a = intval(readline());
+    $b = intval(readline());
+    $c = intval(readline());
+    $d = intval(readline());
+    
+    $DIFERENCA = ($a * $b - $c * $d);
+    echo "DIFERENCA = " . $DIFERENCA . "\n";
+?>
