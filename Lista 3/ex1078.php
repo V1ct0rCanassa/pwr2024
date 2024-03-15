9253526ea98769e22cf7d88ff10827033db91e28
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2024-03-15 15:28:36 -0300

    Create ex1078.php

---
+++ Lista 3/ex1078.php
@@ -0,0 +1,10 @@
+<?php
+    $n = intval(readline());
+    
+    for($i = 1; $i <= 10; $i++){
+        $total = $i * $n;
+        echo $i . " x " .  $n . " = " . $total . "\n"; 
+    }
+    
+    
+?>
